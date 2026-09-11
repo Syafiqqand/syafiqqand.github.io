3d7fda0666274949c180f07518c54c574d4c2783
Syafiqqand <msyafiqa18@gmail.com>
2026-09-11 23:58:58 +0700

    update theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -153,27 +153,6 @@
     });
   }
 
-  /**
-   * Animate the skills items on reveal
-   */
-  let skillsAnimation = document.querySelectorAll(".skills-animation");
-  skillsAnimation.forEach(item => {
-    new Waypoint({
-      element: item,
-      offset: "80%",
-      handler: function (direction) {
-        let progress = item.querySelectorAll(".progress .progress-bar");
-        progress.forEach(el => {
-          el.style.width = el.getAttribute("aria-valuenow") + "%";
-        });
-      },
-    });
-  });
-
-  /**
-   * Initiate Pure Counter
-   */
-  new PureCounter();
 
   /**
    * Init swiper sliders
